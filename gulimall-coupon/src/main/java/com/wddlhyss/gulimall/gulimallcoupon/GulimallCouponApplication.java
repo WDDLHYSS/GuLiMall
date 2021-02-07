8ea9a993191ef467e1b2c083356564c1b3de3a4d
wddlhyss <lhy11235813@163.com>
2021-02-07 21:22:32 +0800

    new project

---
+++ gulimall-coupon/src/main/java/com/wddlhyss/gulimall/gulimallcoupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.wddlhyss.gulimall.gulimallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
